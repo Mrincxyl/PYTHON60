68f009cc37489e41bb68578bff7db8bf32ff3030
Rounak <rounakbasak.personal@gmail.com>
2026-03-08 11:44:12 +0530

    add register code

--- project/UserAuth/views.py
+++ project/UserAuth/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
+
+def Register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST.get('useremail')
+        password = request.POST.get('password')
+
+        
+        alreadyExists = User.objects.filter(username=username).exists()
+
+        if alreadyExists:
+            messages.error(request, 'Username already exists')
+            return render(request, 'register.html')
+        else:
+            new_user = User.objects.create_user(username=username, email=email, password=password)
+            new_user.save()
+            messages.success(request, 'User registered successfully')
+            return render(request, 'login.html')
+
+    return render(request, 'register.html')
+
 def Login(request):
     return render(request, 'login.html')
 
-def Register(request):
-    return render(request, 'register.html')
